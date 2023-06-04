bdaa2e41e5e7b895e38979307ac8375c8cdf1c59
Mateo <mateoquirosaspron@gmail.com>
2023-06-04 16:21:35 -0600

    forgot to change the name of variable l to length

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -6,11 +6,11 @@ def main(*argv):
     i = 0
     length = len(sys.argv) - 1
     if length == 1:
-        print("{:d} argument:".format(l))
+        print("{:d} argument:".format(length))
     elif length == 0:
-        print("{:d} arguments.".format(l))
+        print("{:d} arguments.".format(length))
     else:
-        print("{:d} arguments:".format(l))
+        print("{:d} arguments:".format(length))
     for args in sys.argv:
         if (i != 0):
             print("{}: {}".format(i, args))
