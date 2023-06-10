f44dd580dccba59108b4fa5b31452417e2c90ec9
Mateo <mateoquirosaspron@gmail.com>
2023-06-09 22:19:19 -0600

    task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for element in row:
-            print(element, end = " ")
-        print()
+    for x in range(len(matrix)):
+        for y in range(len(matrix[x])):
+            print('{:d}'.format(matrix[x][y]), end='')
+            if y is not (len(matrix[x]) - 1):
+                print(end=" ")
+        print("")
