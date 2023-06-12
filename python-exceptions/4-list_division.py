3adddb9a0f7a196869547306edc66540a7fea4ca
Mateo <mateoquirosaspron@gmail.com>
2023-06-11 21:06:07 -0600

    task 4

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
-def safe_print_division(a, b):
-    try:
-        x = a / b
-    except:
-        x = None
-    finally:
-        print("Inside result: {}".format(x))
-        return x
+def list_division(my_list_1, my_list_2, list_length):
+    result = []
+    for x in range(0, list_length):
+        try:
+            result.append(my_list_1[x] / my_list_2[x])
+        except IndexError:
+            print("out of range")
+            result.append(0)
+        except TypeError:
+            print("wrong type")
+            result.append(0)
+        except ZeroDivisionError:
+            print("division by 0")
+            result.append(0)
+        finally:
+            pass
+    return result
