2f0d64eafe645de65422c921fc3a9c843e6379d1
Mateo <mateoquirosaspron@gmail.com>
2023-06-16 12:14:44 -0600

    corrected pycodestyle

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+"""Module Square"""
+
 
 class Square:
+    """Square class defined by geometric shape. Attributes: size(int)"""
+
     def __init__(self, size):
+        """Initialize method. Args: size(int)"""
         self.__size = size
