cc4b6c5be6a294975ebc847b2b876065ade9dc66
Mateo <mateoquirosaspron@gmail.com>
2023-06-16 12:32:01 -0600

    task 3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -7,11 +7,12 @@ class Square:
 
     def __init__(self, size=0):
         """Initialize method"""
-        self.__size = size
         if type(size) != int:
             raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
+        else:
+            self.__size = size
     def area(self):
         """Area method: Returns the square's area"""
         return (self.__size**2)
