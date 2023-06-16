057f5b1449664ac05afe33250addcc2a15cc3406
Mateo <mateoquirosaspron@gmail.com>
2023-06-16 12:21:57 -0600

    task 2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,10 +5,10 @@
 class Square:
     """Square class defined by geometric shape. Attributes: size(int)"""
 
-    def __init__(self, size):
+    def __init__(self, size=0):
         """Initialize method. Args: size(int)"""
         self.__size = size
         if type(size) != int:
             raise TypeError('size must be an integer')
         if size < 0:
-            raise ValueError('size must be >= 0'
+            raise ValueError('size must be >= 0')
