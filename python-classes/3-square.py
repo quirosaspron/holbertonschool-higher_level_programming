9a5b70bba9e21d4386f7c18fa9f1528bb50e47a9
Mateo <mateoquirosaspron@gmail.com>
2023-06-16 12:29:58 -0600

    task 3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -14,4 +14,4 @@ class Square:
             raise ValueError('size must be >= 0')
     def area(self):
         """Area method: Returns the square's area"""
-        return (size**2)
+        return (self.__size**2)
