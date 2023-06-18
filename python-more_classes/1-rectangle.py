34258e17c952e59db2f7105088367a2ca81212f8
Mateo <mateoquirosaspron@gmail.com>
2023-06-17 20:13:53 -0600

    task 1

---
+++ python-more_classes/1-rectangle.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+""" Module Rectangle """
+
+
+class Rectangle:
+    """Rectangle class definded by width and height"""
+
+    def __init__(self, width=0, height=0):
+        """Initialize method"""
+        if type(width) != int:
+            raise TypeError('width must be an integer')
+        if type(height) != int:
+            raise TypeError('height must be an integer')
+        else:
+            self.__width = width
+            self.__height = height
+
+    @property
+    def width(self):
+        """Get the width"""
+        return(self.__width)
+
+    @width.setter
+    def width(self, value):
+        """Set the width"""
+        if type(value) != int:
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        else:
+           self.__width = value
+
+    @property
+    def height(self):
+        """Get the height"""
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        """Set the height"""
+        if type(value) != int:
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        else:
+            self.__height = value
