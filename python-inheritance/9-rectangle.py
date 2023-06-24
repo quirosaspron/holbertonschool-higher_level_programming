f4c55975b581fbb91285b9f64b5c98064fd06bea
Mateo <mateoquirosaspron@gmail.com>
2023-06-24 13:36:56 -0600

    task 9

---
+++ python-inheritance/9-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""  Rectangle module """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+
+class Rectangle(BaseGeometry):
+    """ Class htat inherits form BaseGeometry """
+
+    def __init__(self, width, height):
+        """ Initializator """
+
+        self.integer_validator('width', width)
+        self.integer_validator('height', height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """ Area method """
+
+        return(self.__width * self.__height)
+
+    def __str__(self):
+        """ Printing method """
+        return (f"[Rectangle] {self.__width}/{self.__height}")
