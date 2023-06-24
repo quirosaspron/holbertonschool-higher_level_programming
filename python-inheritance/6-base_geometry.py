6caa5e4298a2d8b84c51a3beb1b5daee9e0e10c3
Mateo <mateoquirosaspron@gmail.com>
2023-06-24 12:52:52 -0600

    task 6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Base geometry module """
+
+
+class BaseGeometry:
+    """ Class with empty implementation of area """
+
+    def area(self):
+    """ An empty implementation of area """
+
+        raise Exception("area() is not implemented")
