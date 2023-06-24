96985a52cb258ed970f89f77ce61a9a2bb56eb06
Mateo <mateoquirosaspron@gmail.com>
2023-06-24 13:14:44 -0600

    corrected task 8

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -7,7 +7,7 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 class Rectangle(BaseGeometry):
     """ Class htat inherits form BaseGeometry """
 
-    __init__(self, width, height):
+    def __init__(self, width, height):
         """ Initializator """
 
         self.integer_validator('width', width)
