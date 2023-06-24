06b6edb46655a360445e043aee317d4bf96b4b7d
Mateo <mateoquirosaspron@gmail.com>
2023-06-24 12:27:49 -0600

    inheritance is useful

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+
+class MyList(list):
+    """Custom class inheriting from list"""
+
+    def print_sorted(self):
+        """Print the list in sorted order"""
+        print(sorted(self))
