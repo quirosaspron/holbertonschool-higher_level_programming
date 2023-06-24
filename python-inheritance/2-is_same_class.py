5bae113c66d92367fdaced50a153d939f4387205
Mateo <mateoquirosaspron@gmail.com>
2023-06-24 12:33:58 -0600

    added documentation

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""" is same class """
+
 
 def is_same_class(obj, a_class):
+    """ It checks if an object is an instace of a class, returns True or False """
     return type(obj) is a_class
