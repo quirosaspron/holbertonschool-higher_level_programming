44ea0f8a191b1bfb92bfc1697d2a1a3157f564f9
Mateo <mateoquirosaspron@gmail.com>
2023-06-27 17:41:23 -0600

    correction

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -11,7 +11,7 @@ class Rectangle:
             raise TypeError('width must be an integer')
         if type(height) != int:
             raise TypeError('height must be an integer')
-        if height < 0: 
+        if height < 0:
             raise ValueError('height must be >= 0')
         if width < 0:
             raise ValueError('width must be >= 0')
