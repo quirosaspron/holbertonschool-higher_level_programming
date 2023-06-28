b58521df3a770705d7d56024145eddf6d65f8dc1
Mateo <mateoquirosaspron@gmail.com>
2023-06-28 10:11:10 -0600

    added static mehtod to json sting

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """ This is the base class, it is a Python
     package and it will be used in all other
     classes of this proyect """
+import json
 
 
 class Base:
@@ -18,3 +19,10 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """ Returns a json representation of a list of dictionaries """
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
