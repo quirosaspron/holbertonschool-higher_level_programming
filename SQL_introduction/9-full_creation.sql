e5b557999819e3031a7b40fa3833f8d5450130db
Mateo <mateoquirosaspron@gmail.com>
2023-07-28 19:56:35 -0600

    forgot to add if not exists

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,6 +1,7 @@
--- Creates a second table and adds rows
-CREATE second_table(id INT, name VARCHAR(256), score INT);
-INSERT INTO second_table (id, name, score) value (1, "John", 10);
-INSERT INTO second_table (id, name, score) value (2, "Alex", 3);
-INSERT INTO second_table (id, name, score) value (3, "Bob", 14);
-INSERT INTO second_table (id, name, score) value (4, "George", 8);
+-- CREATE SECOND TABLE and insert into
+CREATE TABLE IF NOT EXISTS second_table (id int, name varchar(256),score int);
+INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
+INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
+INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
+INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);
+ 
