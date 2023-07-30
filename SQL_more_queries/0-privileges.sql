454f0b21fede19ad8fdf7cceb9966c74ea861f55
Mateo <mateoquirosaspron@gmail.com>
2023-07-30 15:25:35 -0600

    should review this later on

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,2 +1,3 @@
-SHOW GRANTS FOR 'user_0d_1'@'localhost'
-SHOW GRANTS FOR 'user_0d_2'@'localhost'
+-- listing the privlage for users 
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
